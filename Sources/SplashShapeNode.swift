998f3d20ac4428a85cc71e0f7b84735b9067e239
f-kubotar <f.kubotar@paperboy.co.jp>
2015-06-30 12:15:30 +0900

    splash update action

--- Sources/SplashShapeNode.swift
+++ Sources/SplashShapeNode.swift
@@ -1,16 +1,46 @@
 import SpriteKit
 
 class SplashShapeNode : SKShapeNode {
+    var size: CGSize
     var wave: Wave
     
-    init(size: CGSize) {
-        self.wave = Wave(size: size, waveLength: 340)
+    private var schedule: SKAction!
+    
+    init(size: CGSize, waveLength: Int = 340) {
+        self.size = size
+        self.wave = Wave(size: size, waveLength: waveLength)
+
         super.init()
         
         self.path = self.wave.createPath()
+        
+        let update = SKAction.runBlock { [weak self] in
+            if let shape = self {
+                shape.wave.updateSprings(0.1)
+                shape.path = shape.wave.createPath()
+            }
+        }
+        let wait = SKAction.waitForDuration(0)
+        self.schedule = SKAction.repeatActionForever(SKAction.sequence([update, wait]))
+        self.runAction(self.schedule)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func splashAt(x: CGFloat, height: CGFloat = 100) {
+        let i = Int(round((x / self.size.width) * CGFloat(self.wave.springs.count)))
+        if i > 0 && i < self.wave.springs.count {
+            self.wave.springs[Int(i)].height = height
+        }
+    }
+    
+    func pausetWave() {
+        self.runAction(self.schedule)
+    }
+    
+    func resumeWave() {
+        self.removeAllActions()
     }
 }
--- Sources/Wave.swift
+++ Sources/Wave.swift
@@ -1,4 +1,4 @@
-import UIKit
+import SpriteKit
 
 class Wave {
     struct Spring {
@@ -10,52 +10,56 @@ class Wave {
             self.height += self.velocity
         }
     }
-
+    
     var size: CGSize
     var density: CGFloat = 0.02
     var rippleSpeed: CGFloat = 0.1
     var springs: [Spring]
+    var numNeighbours = 8
+    
+    private var leftDeltas = [CGFloat]()
+    private var rightDeltas = [CGFloat]()
     
     init(size: CGSize, waveLength: Int = 340) {
         self.size = size
         self.springs = [Spring](count: waveLength, repeatedValue: Spring())
+        self.leftDeltas = [CGFloat](count: self.springs.count, repeatedValue: 0)
+        self.rightDeltas = [CGFloat](count: self.springs.count, repeatedValue: 0)
     }
-
+    
     func updateSprings(spread: CGFloat) {
-        for i in 0..<self.springs.count {
+        let count = self.springs.count
+        for i in 0..<count {
             self.springs[i].update(self.density, rippleSpeed: self.rippleSpeed)
         }
         
-        var leftDeltas = [CGFloat](count: self.springs.count, repeatedValue: 0)
-        var rightDeltas = [CGFloat](count: self.springs.count, repeatedValue: 0)
-        
-        for t in 0..<8 {
-            for i in 0..<self.springs.count {
+        for t in 0..<self.numNeighbours {
+            self.leftDeltas[0] = 0
+            self.leftDeltas[count - 1] = 0
+            
+            self.rightDeltas[0] = 0
+            self.rightDeltas[count - 1] = 0
+            
+            // var leftDeltas = [CGFloat](count: count, repeatedValue: 0)
+            // var rightDeltas = [CGFloat](count: count, repeatedValue: 0)
+
+            for i in 0..<count {
                 if i > 0 {
                     leftDeltas[i] = spread * (self.springs[i].height - self.springs[i - 1].height)
-
-                    var spring = self.springs[i - 1]
-                    spring.velocity += leftDeltas[i]
-                    self.springs[i - 1] = spring
+                    self.springs[i - 1].velocity += leftDeltas[i - 1]
                 }
                 if i < self.springs.count - 1 {
                     rightDeltas[i] = spread * (self.springs[i].height - self.springs[i + 1].height)
-                    var spring = self.springs[i + 1]
-                    spring.velocity += rightDeltas[i]
-                    self.springs[i + 1] = spring
+                    self.springs[i + 1].velocity += rightDeltas[i]
                 }
             }
             
-            for i in 0..<self.springs.count {
+            for i in 0..<count {
                 if i > 0 {
-                    var spring = self.springs[i - 1]
-                    spring.height += leftDeltas[i]
-                    self.springs[i - 1] = spring
+                    self.springs[i - 1].height += leftDeltas[i]
                 }
-                if i < self.springs.count - 1 {
-                    var spring = self.springs[i + 1]
-                    spring.height += rightDeltas[i]
-                    self.springs[i + 1] = spring
+                if i < count - 1 {
+                    self.springs[i + 1].height += rightDeltas[i]
                 }
             }
         }
@@ -66,9 +70,9 @@ class Wave {
         path.moveToPoint(CGPoint(x: 0, y: 0))
         for i in 0..<self.springs.count {
             let spring = self.springs[i]
-
+            
             let x = CGFloat(i) * (self.size.width / CGFloat(self.springs.count))
-            let y = self.size.height * 0.5 - spring.height
+            let y = self.size.height - spring.height
             path.addLineToPoint(CGPoint(x: x, y: y))
         }
         path.addLineToPoint(CGPoint(x: self.size.width, y: 0))
